db942d32ef9e46e7a4ea497f171ec66686f105e8
Heather Cartwright <heather.cartwright@avast.com>
2021-10-15 14:44:26 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,7 +21,7 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException();
 
             // find 20% of students
-            int gradeBoundaryNumber = (int)Math.Ceiling((double)(studentCount / 5));
+            int threshold = (int)Math.Ceiling(studentCount * 0.2);
 
             // sort the grades
             List<Student> sortedList = Students;
@@ -31,15 +31,16 @@ namespace GradeBook.GradeBooks
             {
                 gradeList.Add(item.AverageGrade);
             }
+            gradeList.Reverse();
 
             // find where averageGrade sits in the list of grades
-            if (averageGrade >= gradeList[gradeBoundaryNumber])
+            if (averageGrade >= gradeList[threshold - 1])
                 return 'A';
-            else if (averageGrade >= gradeList[gradeBoundaryNumber * 2])
+            else if (averageGrade >= gradeList[(threshold * 2) - 1])
                 return 'B';
-            else if (averageGrade >= gradeList[gradeBoundaryNumber * 3])
+            else if (averageGrade >= gradeList[(threshold * 3) - 1])
                 return 'C';
-            else if (averageGrade >= gradeList[gradeBoundaryNumber * 4])
+            else if (averageGrade >= gradeList[(threshold * 4) - 1])
                 return 'D';
             else
                 return 'F';
